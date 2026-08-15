c888733b38e5df0b755eeeae4275aa68307aba68
rbirda <51870669+rbirda@users.noreply.github.com>
2026-08-15 11:01:17 +0530

    Add: remove-element - submission-4

---
+++ Data Structures & Algorithms/remove-element/submission-4.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int RemoveElement(int[] nums, int val) {
+        int countMatchingValue = 0;
+        if(nums.Length == 0)
+           return 0;
+        
+        if(!nums.Contains(val))
+            return nums.Length ;
+
+        for(int i=0; i < nums.Length;i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[countMatchingValue] = nums[i];
+                countMatchingValue++;
+            }
+
+        }
+        return countMatchingValue;
+        
+    }
+}
