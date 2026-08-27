5ef0ee5e6168f649eb53a7eecee414f11bd0d09e
rbirda <51870669+rbirda@users.noreply.github.com>
2026-08-27 18:51:10 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.cs
@@ -0,0 +1,29 @@
+public class Solution {
+     public int ClimbStairs(int n) {
+
+       if (n <= 2) return n;
+
+    int prev1 = 1;
+    int prev2 = 2;
+
+    for (int i = 3; i <= n; i++)
+    {
+        int current = prev1 + prev2;
+        prev1 = prev2;
+        prev2 = current;
+    }
+
+    return prev2;
+  
+
+
+
+//     public int ClimbStairs(int n) {
+//         return Dfs(n, 0);
+//     }
+//     public int Dfs(int n, int i) {
+//         if (i >= n)
+//             return i == n ? 1 : 0;
+//         return Dfs(n, i + 1) + Dfs(n, i + 2);
+    }
+ }
