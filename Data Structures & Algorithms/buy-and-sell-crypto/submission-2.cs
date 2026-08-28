965aa146a4221e372ab5a03e61aa94cfc293ee19
rbirda <51870669+rbirda@users.noreply.github.com>
2026-08-28 20:12:32 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        int maxProfit = 0;
+        int minPrice = prices[0];
+
+        for (int i = 1; i < prices.Length; i++) {
+
+ maxProfit = Math.Max(maxProfit, prices[i]- minPrice);
+           
+            if(prices[i] < minPrice)
+            {
+           
+              minPrice = prices[i];
+            }
+        }
+
+        return maxProfit;
+    }
+}
